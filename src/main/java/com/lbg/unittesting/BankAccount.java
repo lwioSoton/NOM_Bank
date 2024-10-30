16f2eb70404ab71844c68bcb0b35c0dc733a5260
lwioSoton <lwi1g19@soton.ac.uk>
2024-10-30 15:59:41 +0000

    EDIT: Added withdrawMoney test and belowMinMoneyReached test

--- src/main/java/com/lbg/unittesting/BankAccount.java
+++ src/main/java/com/lbg/unittesting/BankAccount.java
@@ -1,4 +1,46 @@
 package com.lbg.unittesting;
 
 public class BankAccount {
+
+    private long balance;
+    private long minBalance;
+    private String accountHolder;
+    public BankAccount(long balance, long minBalance, String accountHolder){
+        this.setBalance(balance);
+        this.setMinBalance(minBalance);
+        this.setAccountHolder(accountHolder);
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    private void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    public long getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(long minBalance) {
+        this.minBalance = minBalance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public boolean withdrawMoney(long amountWithdrawled){
+        if ((this.balance - amountWithdrawled) < minBalance)
+            return false;
+        else {
+            this.setBalance(this.balance - amountWithdrawled);
+            return true;
+        }
+    }
 }
--- src/test/java/com/lbg/unittesting/BankAccountTest.java
+++ src/test/java/com/lbg/unittesting/BankAccountTest.java
@@ -1,4 +1,40 @@
 package com.lbg.unittesting;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankAccountTest {
+    @Test
+    public void testWithdrawMoney(){
+        BankAccount b1 = new BankAccount(1000000, 0, "Victoria Lloyd");
+        b1.withdrawMoney(1);
+        assertEquals(999999, b1.getBalance());
+    }
+    @Test
+    public void testBelowMinMoneyReached(){
+        BankAccount b1 = new BankAccount(1000000, 0, "Victoria Lloyd");
+        boolean ifMoneyWithdrawed = b1.withdrawMoney(1000001);  //Withdrawing below 0.
+        assertEquals(false, ifMoneyWithdrawed);
+        // Expecting a boolean to return false if Below Miniumum Money is reached.
+    }
+    @Test
+    public void testGetBalance(){
+        BankAccount b1 = new BankAccount(1000000, 0, "Victoria Lloyd");
+
+    }
+    @Test
+    public void testSetBalance(){
+        BankAccount b1 = new BankAccount(1000000, 0, "Victoria Lloyd");
+
+    }
+    @Test
+    public void testGetSetName(){
+        BankAccount b1 = new BankAccount(1000000, 0, "Victoria Lloyd");
+
+    }
+    @Test
+    public void testSetName(){
+        BankAccount b1 = new BankAccount(1000000, 0, "Victoria Lloyd");
+
+    }
 }
